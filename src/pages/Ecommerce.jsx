142fd9321ef5a92995c166cf1545a26c2c7477fd
Beno Suriano <bjsuriano@gmail.com>
2022-07-30 20:00:42 -0400

    Display revenue update "budget" summary.

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -81,6 +81,7 @@ const Ecommerce = () => {
                     <span className='text-3xl font-semibold'>$93,438</span>
                     <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
                   </p>
+                  <p className='text-gray-500 mt-1'>Budget</p>
                   </div>
                 </div>
             </div>
