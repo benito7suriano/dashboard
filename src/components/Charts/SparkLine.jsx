dedbc432bfd67b01046f3803483d38ff7852f08b
Beno Suriano <bjsuriano@gmail.com>
2022-08-02 11:22:44 -0400

    Construct orders table.

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -5,28 +5,30 @@ import {
   SparklineTooltip,
 } from '@syncfusion/ej2-react-charts'
 
-const Sparkline = ({ currentColor, id, type, height, width, data, color }) => (
-  <SparklineComponent
-    id={id}
-    height={height}
-    width={width}
-    lineWidth={1}
-    valueType='Numeric'
-    fill={color}
-    border={{ color: currentColor, width: 2 }}
-    dataSource={data}
-    xName='x'
-    yName='yval'
-    type={type}
-    tooltipSettings={{
-      visible: true,
-      format: '${x}: data ${yval}',
-      trackLineSettings: {
+const Sparkline = ({ currentColor, id, type, height, width, data, color }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType='Numeric'
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName='x'
+      yName='yval'
+      type={type}
+      tooltipSettings={{
         visible: true,
-      },
-    }}>
-    <Inject services={[SparklineTooltip]} />
-  </SparklineComponent>
-)
+        format: '${x}: data ${yval}',
+        trackLineSettings: {
+          visible: true,
+        },
+      }}>
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  )
+}
 
 export default Sparkline
