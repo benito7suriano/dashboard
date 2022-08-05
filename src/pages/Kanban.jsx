4231a1d277ef5b92bfa54d57601d563843e4038a
Beno Suriano <bjsuriano@gmail.com>
2022-08-05 11:30:58 -0400

    Display column headers for kanban.

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -12,6 +12,13 @@ const Kanban = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='App' title='Kanban' />
+      <KanbanComponent>
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => {
+            return <ColumnDirective key={index} {...item} />
+          })}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   )
 }
