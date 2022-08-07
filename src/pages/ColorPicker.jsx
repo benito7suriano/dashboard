9de703e45516e09022226a568fab099f792a4d8a
Beno Suriano <bjsuriano@gmail.com>
2022-08-07 15:38:52 -0400

    Inline palette component for color-picker page.

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -10,6 +10,18 @@ const ColorPicker = () => {
         <Header category='App' title='Color Picker' />
         <div className='text-center'>
           <div id='preview' />
+          <div className='flex justify-center items-center gap-20 flex-wrap'>
+            <div>
+              <p className='text-2xl font-semibold mt-2 mb-4'>Inline Palette</p>
+              <ColorPickerComponent
+                id='inline-palette'
+                mode='Palette'
+                modeSwitcher={false}
+                inline
+                showButtons={false}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
