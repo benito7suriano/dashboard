f99642c04c3fdd433a739a0b4fffc1ca284b8ea8
Beno Suriano <bjsuriano@gmail.com>
2022-08-07 15:36:57 -0400

    Pencil preview for color picker page.

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -8,6 +8,9 @@ const ColorPicker = () => {
     <div>
       <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
         <Header category='App' title='Color Picker' />
+        <div className='text-center'>
+          <div id='preview' />
+        </div>
       </div>
     </div>
   )
