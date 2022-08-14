67888746420180350d38a2f19e49f1984b01a928
Beno Suriano <bjsuriano@gmail.com>
2022-08-14 13:01:17 -0400

    Render Color Mapping card layout.

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  ColumnSeries,
+  Category,
+  Tooltip,
+  Legend,
+  RangeColorSettingsDirective,
+  RangeColorSettingDirective,
+} from '@syncfusion/ej2-react-charts'
+
+import {
+  colorMappingData,
+  ColorMappingPrimaryXAxis,
+  ColorMappingPrimaryYAxis,
+} from '../../data/dummy'
+import { Header } from '../../components'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 const ColorMapping = () => {
+  const { currentMode } = useStateContext()
+
   return (
-    <div>ColorMapping</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header
+        category='Color Mappping'
+        title='USA CLIMATE - WEATHER BY MONTH'
+      />
+      <div className='w-full'></div>
+    </div>
   )
 }
 
