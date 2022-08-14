046f293a2ae2f21bdf09857737b81284012b08f0
Beno Suriano <bjsuriano@gmail.com>
2022-08-13 22:37:06 -0400

    Import Header component for Financial graph.

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Header } from '../../components'
+
 const Financial = () => {
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
