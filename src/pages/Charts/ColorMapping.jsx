e91c38489d79bd0f754ff1a5f3c6a0221f7bae0e
Beno Suriano <bjsuriano@gmail.com>
2022-08-14 13:07:58 -0400

    Render color map for temperature ranges in US.

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -16,6 +16,7 @@ import {
   colorMappingData,
   ColorMappingPrimaryXAxis,
   ColorMappingPrimaryYAxis,
+  rangeColorMapping,
 } from '../../data/dummy'
 import { Header } from '../../components'
 import { useStateContext } from '../../contexts/ContextProvider'
@@ -29,7 +30,34 @@ const ColorMapping = () => {
         category='Color Mappping'
         title='USA CLIMATE - WEATHER BY MONTH'
       />
-      <div className='w-full'></div>
+      <div className='w-full'>
+        <ChartComponent
+          id='color-mapping-chart'
+          primaryXAxis={ColorMappingPrimaryXAxis}
+          primaryYAxis={ColorMappingPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          legendSettings={{ mode: 'Range', background: 'white' }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}>
+          <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={colorMappingData[0]}
+              name='USA'
+              xName='x'
+              yName='y'
+              type='Column'
+              cornerRadius={{ topLeft: 10, topRight: 10 }}
+            />
+          </SeriesCollectionDirective>
+          <RangeColorSettingsDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {rangeColorMapping.map((item, index) => (
+              <RangeColorSettingDirective key={index} {...item} />
+            ))}
+          </RangeColorSettingsDirective>
+        </ChartComponent>
+      </div>
     </div>
   )
 }
